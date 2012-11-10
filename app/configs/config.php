89b9cbb04c58abfde612038ef140f6d490be3d17
rick@rickstevens.nl <rick@rickstevens.nl@b76d4d87-7c65-3e3d-d836-a64cab6a3ca6>
2012-11-10 15:04:07 +0000

    Removed magic_quotes disabler (legacy)

--- app/configs/config.php
+++ app/configs/config.php
@@ -9,7 +9,8 @@
 // TRUE: display_errors, FALSE: log_errors (/system/tmp/logs/error.log)
 $config['development'] = TRUE;
 
-// (optional) Uncomment and change this to your website's URL + subfolders (see /.htaccess for additional subfolder settings)
+// (optional) Uncomment and change this to your website's URL + subfolder(s) (see /.htaccess for additional subfolder settings)
+// No trailing slash!
 #$config['root'] = 'http://example.com/subfolder';
 
 // (optional) Uncomment these to enable PDO database access in models through $this->db
