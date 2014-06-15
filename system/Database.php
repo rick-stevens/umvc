b08482c52df8b2719650d998166b4c72818f9508
Rick Stevens <github@rickstevens.nl>
2014-06-15 12:25:38 +0200

    Minor code cleanup

--- system/Database.php
+++ system/Database.php
@@ -5,15 +5,15 @@
 
 class Database
 {
-	private static $instance = NULL;
+	private static $_instance = NULL;
 
 	private function __construct() {}
 
 	public static function getInstance()
 	{
-		if ( ! isset(self::$instance) && isset(MVC::$config['db'])) {
+		if ( ! isset(self::$_instance) && isset(MVC::$config['db'])) {
 			try {
-				self::$instance = new PDOWrapper(
+				self::$_instance = new PDOWrapper(
 					'mysql:host=' . MVC::$config['db']['host'] . ';dbname=' . MVC::$config['db']['database'] . ';charset=utf8',
 					MVC::$config['db']['username'],
 					MVC::$config['db']['password']
@@ -26,6 +26,6 @@ class Database
 			unset(MVC::$config['db']);
 		}
 
-		return self::$instance;
+		return self::$_instance;
 	}
 }
